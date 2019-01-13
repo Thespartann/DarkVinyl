anshuman852/Phonograph

Add Vanilla Tag Editor compatibility
just install the app from https://play.google.com/store/apps/details?id=com.kanedias.vanilla.audiotag
then Phonograph will use it as default tag editor for songs (album editor will be still the stock
Phonograph one)

app/src/main/java/com/kabouzeid/gramophone/helper/menu/SongMenuHelper.java
@@ -1,6 +1,7 @@
 package com.kabouzeid.gramophone.helper.menu;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -16,9 +17,12 @@ import com.kabouzeid.gramophone.helper.MusicPlayerRemote;
 import com.kabouzeid.gramophone.interfaces.PaletteColorHolder;
 import com.kabouzeid.gramophone.model.Song;
 import com.kabouzeid.gramophone.ui.activities.tageditor.AbsTagEditorActivity;
+import com.kabouzeid.gramophone.ui.activities.tageditor.PluginUtils;
 import com.kabouzeid.gramophone.ui.activities.tageditor.SongTagEditorActivity;
 import com.kabouzeid.gramophone.util.MusicUtil;
 import com.kabouzeid.gramophone.util.NavigationUtil;
+
+import java.io.File;
 
 /**
  * @author Karim Abou Zeid (kabouzeid)
@@ -47,12 +51,26 @@ public class SongMenuHelper {
                 MusicPlayerRemote.enqueue(song);
                 return true;
             case R.id.action_tag_editor:
-                Intent tagEditorIntent = new Intent(activity, SongTagEditorActivity.class);
-                tagEditorIntent.putExtra(AbsTagEditorActivity.EXTRA_ID, song.id);
-                if (activity instanceof PaletteColorHolder)
-                    tagEditorIntent.putExtra(AbsTagEditorActivity.EXTRA_PALETTE, ((PaletteColorHolder) activity).getPaletteColor());
-                activity.startActivity(tagEditorIntent);
+
+                if (PluginUtils.checkPlugins(activity)) {
+                    Intent request = new Intent(PluginUtils.ACTION_LAUNCH_PLUGIN);
+                    request.setPackage("com.kanedias.vanilla.audiotag");
+                    request.putExtra(PluginUtils.EXTRA_PARAM_URI, Uri.fromFile(new File(song.data)));
+                    request.putExtra(PluginUtils.EXTRA_PARAM_SONG_TITLE, song.title);
+                    request.putExtra(PluginUtils.EXTRA_PARAM_SONG_ARTIST, song.artistName);
+                    request.putExtra(PluginUtils.EXTRA_PARAM_SONG_ALBUM, song.albumName);
+                    activity.startService(request);
+                } else {
+                    Intent tagEditorIntent = new Intent(activity, SongTagEditorActivity.class);
+                    tagEditorIntent.putExtra(AbsTagEditorActivity.EXTRA_ID, song.id);
+                    if (activity instanceof PaletteColorHolder)
+                        tagEditorIntent.putExtra(AbsTagEditorActivity.EXTRA_PALETTE, ((PaletteColorHolder) activity).getPaletteColor());
+                    activity.startActivity(tagEditorIntent);
+                }
                 return true;
+
+
+
             case R.id.action_details:
                 SongDetailDialog.create(song).show(activity.getSupportFragmentManager(), "SONG_DETAILS");
                 return true;

app/src/main/java/com/kabouzeid/gramophone/ui/activities/tageditor/PluginUtils.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright (C) 2016 Oleg Chernovskiy <adonai@xaker.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.kabouzeid.gramophone.ui.activities.tageditor;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
+
+/**
+ * Common plugin utilities and constants reside here.
+ * Note that public `action` and `extra` strings are common over all plugins, so please keep them in sync.
+ */
+
+public class PluginUtils {
+
+    private PluginUtils() {
+    }
+
+    // these actions are for passing between main player and plugins
+    public static final String ACTION_REQUEST_PLUGIN_PARAMS = "ch.blinkenlights.android.vanilla.action.REQUEST_PLUGIN_PARAMS"; // broadcast
+    //public static final String ACTION_HANDLE_PLUGIN_PARAMS = "ch.blinkenlights.android.vanilla.action.HANDLE_PLUGIN_PARAMS"; // answer
+    public static final String ACTION_WAKE_PLUGIN = "ch.blinkenlights.android.vanilla.action.WAKE_PLUGIN"; // targeted at each found
+    public static final String ACTION_LAUNCH_PLUGIN = "ch.blinkenlights.android.vanilla.action.LAUNCH_PLUGIN"; // targeted at selected by user
+
+    /* these are used by plugins to describe themselves
+    public static final String EXTRA_PARAM_PLUGIN_NAME = "ch.blinkenlights.android.vanilla.extra.PLUGIN_NAME";
+    public static final String EXTRA_PARAM_PLUGIN_APP = "ch.blinkenlights.android.vanilla.extra.PLUGIN_APP";
+    public static final String EXTRA_PARAM_PLUGIN_DESC = "ch.blinkenlights.android.vanilla.extra.PLUGIN_DESC";*/
+
+    // this is passed to plugin when it is selected by user
+    public static final String EXTRA_PARAM_URI = "ch.blinkenlights.android.vanilla.extra.URI";
+    public static final String EXTRA_PARAM_SONG_TITLE = "ch.blinkenlights.android.vanilla.extra.SONG_TITLE";
+    public static final String EXTRA_PARAM_SONG_ALBUM = "ch.blinkenlights.android.vanilla.extra.SONG_ALBUM";
+    public static final String EXTRA_PARAM_SONG_ARTIST = "ch.blinkenlights.android.vanilla.extra.SONG_ARTIST";
+
+    //static final String EXTRA_PLUGIN_MAP = "ch.blinkenlights.android.vanilla.internal.extra.PLUGIN_MAP";
+
+    public static boolean checkPlugins(Context ctx) {
+        List<ResolveInfo> resolved = ctx.getPackageManager().queryBroadcastReceivers(new Intent(ACTION_REQUEST_PLUGIN_PARAMS), 0);
+        if(!resolved.isEmpty()) {
+            // If plugin is just installed, Android will not deliver intents to its receiver
+            // until it's started at least one time
+            for (ResolveInfo ri : resolved) {
+                Intent awaker = new Intent(ACTION_WAKE_PLUGIN);
+                awaker.setPackage(ri.activityInfo.packageName);
+                ctx.startService(awaker);
+            }
+            return true;
+        }
+
+        return false;
+    }
+}

Thespartann/DarkVinyl

Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

app/src/main/java/com/poupa/vinylmusicplayer/helper/menu/SongMenuHelper.java
@@ -1,6 +1,7 @@
 package com.poupa.vinylmusicplayer.helper.menu;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -16,9 +17,12 @@ import com.poupa.vinylmusicplayer.helper.MusicPlayerRemote;
 import com.poupa.vinylmusicplayer.interfaces.PaletteColorHolder;
 import com.poupa.vinylmusicplayer.model.Song;
 import com.poupa.vinylmusicplayer.ui.activities.tageditor.AbsTagEditorActivity;
+import com.poupa.vinylmusicplayer.ui.activities.tageditor.PluginUtils;
 import com.poupa.vinylmusicplayer.ui.activities.tageditor.SongTagEditorActivity;
 import com.poupa.vinylmusicplayer.util.MusicUtil;
 import com.poupa.vinylmusicplayer.util.NavigationUtil;
+
+import java.io.File;
 
 /**
  * @author Karim Abou Zeid (kabouzeid)
@@ -47,12 +51,27 @@ public class SongMenuHelper {
                 MusicPlayerRemote.enqueue(song);
                 return true;
             case R.id.action_tag_editor:
-                Intent tagEditorIntent = new Intent(activity, SongTagEditorActivity.class);
-                tagEditorIntent.putExtra(AbsTagEditorActivity.EXTRA_ID, song.id);
-                if (activity instanceof PaletteColorHolder)
-                    tagEditorIntent.putExtra(AbsTagEditorActivity.EXTRA_PALETTE, ((PaletteColorHolder) activity).getPaletteColor());
-                activity.startActivity(tagEditorIntent);
+                
+                if (PluginUtils.checkPlugins(activity)) {
+                    Intent request = new Intent(PluginUtils.ACTION_LAUNCH_PLUGIN);
+                    request.setPackage("com.kanedias.vanilla.audiotag");
+                    request.putExtra(PluginUtils.EXTRA_PARAM_URI, Uri.fromFile(new File(song.data)));
+                    request.putExtra(PluginUtils.EXTRA_PARAM_SONG_TITLE, song.title);
+                    request.putExtra(PluginUtils.EXTRA_PARAM_SONG_ARTIST, song.artistName);
+                    request.putExtra(PluginUtils.EXTRA_PARAM_SONG_ALBUM, song.albumName);
+                    activity.startService(request);
+                } else {
+                    Intent tagEditorIntent = new Intent(activity, SongTagEditorActivity.class);
+                    tagEditorIntent.putExtra(AbsTagEditorActivity.EXTRA_ID, song.id);
+                    if (activity instanceof PaletteColorHolder)
+                        tagEditorIntent.putExtra(AbsTagEditorActivity.EXTRA_PALETTE, ((PaletteColorHolder) 
+activity).getPaletteColor());
+                    activity.startActivity(tagEditorIntent);
+                }
                 return true;
+                
+                
+                
             case R.id.action_details:
                 SongDetailDialog.create(song).show(activity.getSupportFragmentManager(), "SONG_DETAILS");
                 return true;

app/src/main/java/com/poupa/vinylmusicplayer/ui/activities/tageditor/PluginUtils.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright (C) 2016 Oleg Chernovskiy <adonai@xaker.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.poupa.vinylmusicplayer.ui.activities.tageditor;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
+
+/**
+ * Common plugin utilities and constants reside here.
+ * Note that public `action` and `extra` strings are common over all plugins, so please keep them in sync.
+ */
+
+public class PluginUtils {
+
+    private PluginUtils() {
+    }
+
+    // these actions are for passing between main player and plugins
+    public static final String ACTION_REQUEST_PLUGIN_PARAMS = "ch.blinkenlights.android.vanilla.action.REQUEST_PLUGIN_PARAMS"; // broadcast
+    //public static final String ACTION_HANDLE_PLUGIN_PARAMS = "ch.blinkenlights.android.vanilla.action.HANDLE_PLUGIN_PARAMS"; // answer
+    public static final String ACTION_WAKE_PLUGIN = "ch.blinkenlights.android.vanilla.action.WAKE_PLUGIN"; // targeted at each found
+    public static final String ACTION_LAUNCH_PLUGIN = "ch.blinkenlights.android.vanilla.action.LAUNCH_PLUGIN"; // targeted at selected by user
+
+    /* these are used by plugins to describe themselves
+    public static final String EXTRA_PARAM_PLUGIN_NAME = "ch.blinkenlights.android.vanilla.extra.PLUGIN_NAME";
+    public static final String EXTRA_PARAM_PLUGIN_APP = "ch.blinkenlights.android.vanilla.extra.PLUGIN_APP";
+    public static final String EXTRA_PARAM_PLUGIN_DESC = "ch.blinkenlights.android.vanilla.extra.PLUGIN_DESC";*/
+
+    // this is passed to plugin when it is selected by user
+    public static final String EXTRA_PARAM_URI = "ch.blinkenlights.android.vanilla.extra.URI";
+    public static final String EXTRA_PARAM_SONG_TITLE = "ch.blinkenlights.android.vanilla.extra.SONG_TITLE";
+    public static final String EXTRA_PARAM_SONG_ALBUM = "ch.blinkenlights.android.vanilla.extra.SONG_ALBUM";
+    public static final String EXTRA_PARAM_SONG_ARTIST = "ch.blinkenlights.android.vanilla.extra.SONG_ARTIST";
+
+    //static final String EXTRA_PLUGIN_MAP = "ch.blinkenlights.android.vanilla.internal.extra.PLUGIN_MAP";
+
+    public static boolean checkPlugins(Context ctx) {
+        List<ResolveInfo> resolved = ctx.getPackageManager().queryBroadcastReceivers(new Intent(ACTION_REQUEST_PLUGIN_PARAMS), 0);
+        if(!resolved.isEmpty()) {
+            // If plugin is just installed, Android will not deliver intents to its receiver
+            // until it's started at least one time
+            for (ResolveInfo ri : resolved) {
+                Intent awaker = new Intent(ACTION_WAKE_PLUGIN);
+                awaker.setPackage(ri.activityInfo.packageName);
+                ctx.startService(awaker);
+            }
+            return true;
+        }
+
+        return false;
+    }
+}